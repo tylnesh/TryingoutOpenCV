e2fe6d656898fc1d6ca18b4b48383cf7a799fbf7
Michal Kohutek <kohutek.michal@gmail.com>
2020-09-06 16:09:56 +0200

    Switched to webcam facial detection

--- opencv_experiments/main.cpp
+++ opencv_experiments/main.cpp
@@ -8,26 +8,40 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    cv::Mat img = cv::imread("/home/tylnesh/Pictures/Birmingham-Xixon/IMG_20180501_015236.jpg",3);
-    cv::Mat greyImg;
-    cv::resize(img,img,cv::Size(800,600),0.5,0.5);
-    cv::cvtColor(img,greyImg,cv::COLOR_BGRA2GRAY);
-    cv::CascadeClassifier faceFrontCascade;
-    faceFrontCascade = cv::CascadeClassifier("/home/tylnesh/opencv/data/haarcascades/haarcascade_frontalface_default.xml");
 
+
+    QCoreApplication a(argc, argv);
+
+
+    //cv::VideoCapture *video = new cv::VideoCapture("/dev/video0");
+    cv::VideoCapture *video = new cv::VideoCapture();
+    video->open(0);
+    cv::Mat img;
+    cv::Mat greyImg;
+    cv::CascadeClassifier faceFrontCascade;
+    faceFrontCascade = cv::CascadeClassifier("/home/tylnesh/opencv_build/opencv/data/haarcascades/haarcascade_frontalface_default.xml");
     std::vector<cv::Rect> faceObjects;
 
+    bool captureCorrect;
+    while(true){
+    captureCorrect = video->read(img);
+    if (!captureCorrect) {
+        qDebug() << "can't receive frame";
+        break;
+    }
+    //cv::Mat img = cv::imread("/home/tylnesh/Pictures/Birmingham-Xixon/IMG_20180501_015236.jpg",3);
+
+    cv::resize(img,img,cv::Size(800,600),0.5,0.5);
+    cv::cvtColor(img,greyImg,cv::COLOR_BGRA2GRAY);
     faceFrontCascade.detectMultiScale(greyImg, faceObjects);
 
-    for (int i = 0; i<faceObjects.size(); i++)
-
-    {
-        //cv::Rect
+    for (size_t i = 0; i<faceObjects.size(); i++){
         cv::Scalar colorScalar = cv::Scalar( 255, 0, 0 );
         cv::rectangle(img,faceObjects.at(i),colorScalar,5);
     }
-    cv::imshow("Obrazok",img);
 
+    cv::imshow("Obrazok",img);
+    if(cv::waitKey(1) == 'q') break;
+    }
     return a.exec();
 }
